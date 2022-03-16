6da18a0929e6292e4e2e1fac88f772e201ec63eb
jokay03J <magrojoan3@gmail.com>
2022-03-16 16:47:16 +0100

    update main file
    changelog:
    -add PROD and DEV	mode
    -modify indentation

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //import module
 const { Client, Intents, Collection } = require('discord.js');
 require("dotenv").config(".env")
-const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 require("colors")
@@ -31,20 +30,31 @@ for (const file of eventFiles) {
   //event file data
   const event = require(`./events/${file}`);
   //check if event is once
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args,client));
-	} else {
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
     console.log(`${event.name}`.yellow);
-		client.on(event.name, (...args) => event.execute(...args,client));
-	}
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
 }
 
 //define REST type with token
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 //set commands in application with REST
-rest.put(Routes.applicationGuildCommands(process.env.APPLICATIONID, process.env.GUILDID), { body: commands })
-  .then(() => console.log('toute les commandes ont bien été charger'.green))
-  .catch(console.error);
+if (process.env.MODE === "PROD") {
+  rest.put(
+    Routes.applicationCommands(process.env.APPLICATIONID), { body: commands })
+    .then(() => console.log("toute les commandes ont bien été charger!".green))
+    .catch(console.error)
+} else {
+  rest.put(
+    Routes.applicationGuildCommands(process.env.APPLICATIONID, process.env.GUILDID), { body: commands })
+    .then(() => {
+      console.log("DEV MODE".red);
+      console.log('toute les commandes ont bien été charger!'.green);
+    })
+    .catch(console.error);
+}
 
 // Login to Discord with your client's token
 client.login(process.env.TOKEN);
